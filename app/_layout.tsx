9a8bdeb4b5f8b671dce87fc74b6ee6ebbe035336
Levi Vitor <leveevitor@hotmail.com>
2026-05-21 08:54:54 -0300

    Fix StatusBar component rendering

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -54,7 +54,7 @@ function RootLayoutNav() {
 
   return (
     <>
-      <StatusBar style={isDark ? "light" : "dark"} />{" "}
+      <StatusBar style={isDark ? "light" : "dark"} />
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(auth)" />
         <Stack.Screen name="(tabs)" />
